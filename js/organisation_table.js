ebc8d46e36edfb3be7005200145a84267ef1bbf6
Roxane Braconnier <rox.braconnier@gmail.com>
2023-05-22 12:13:30 +0200

    formulaire + anciennes commandes  => cest moche mais ça marche

--- js/organisation_table.js
+++ js/organisation_table.js
@@ -151,3 +151,170 @@ function updateTotalPrice() {
   }
   document.querySelector('.cart-total-price').innerText = total + '€';
 }
+
+
+
+function Open_old_orders() {
+  console.log("haha")
+  // Get the .cart element
+  let order = document.querySelector(".ancienne-commandes");
+  console.log(order)
+  // Add the class 'active' to the .cart element
+  order.classList.add("active")
+}
+
+function Close_old_orders() {
+  console.log("haha")
+  // Get the .cart element
+  let order = document.querySelector(".ancienne-commandes");
+  console.log(order)
+  // Add the class 'active' to the .cart element
+  order.classList.remove("active");
+}
+
+let commandCount = 0; // Keep track of the number of commands
+function buy() {
+  let form =createForm();
+  Open_formulaire();
+
+  let cartContent = document.querySelector(".cart-content");
+
+  let titles = [];
+  let titleElements = document.querySelectorAll('.cart-product-title');
+  titleElements.forEach(function (element) {
+      titles.push(element.textContent);
+  });
+  let pastOrders=titles;
+  console.log(pastOrders)
+
+  let list = document.createElement('ul');
+  list.style.display = "none";
+  pastOrders.forEach(function (item) {
+      let li = document.createElement('li');
+      li.textContent = item;
+      list.appendChild(li);
+  });
+
+    // Add a submit event listener to the form
+    form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Get the values of the form fields
+    let nom = document.querySelector('#nom').value;
+    let prenom = document.querySelector('#prenom').value;
+    let telephone = document.querySelector('#telephone').value;
+    let email = document.querySelector('#email').value;
+    
+    // Create a new element to display the coordinates
+    let coordinatesElement = document.createElement('div');
+    coordinatesElement.innerHTML = `
+    My data : <br>
+      Nom: ${nom}; 
+      Prénom: ${prenom};
+      Téléphone: ${telephone};
+      Email: ${email}
+    `;
+      list.appendChild(coordinatesElement);
+
+      while (cartContent.hasChildNodes()) {
+        cartContent.removeChild(cartContent.firstChild);
+    }
+    updateTotalPrice();
+    Close_formulaire()
+  });
+   
+  // Get the cart element and the list of old command
+  let oldCommands = document.querySelector(".ancienne-commandes");
+    let newCommand = document.createElement("li");
+    let commandText = document.createElement("span");
+    commandText.textContent = "Command " + commandCount;
+    newCommand.appendChild(commandText);
+    newCommand.appendChild(list);
+    newCommand.addEventListener("click", function() {
+      if (list.style.display === "none") {
+          list.style.display = "block";
+      } else {
+          list.style.display = "none";
+      }
+  });
+  commandCount++;
+  oldCommands.appendChild(newCommand);
+  updateTotalPrice();
+}
+
+//create the form 
+function createForm() {
+  let formbox = document.getElementsByClassName('formulaire')[0];
+  let form = document.createElement('form');
+  
+  let labelNom = document.createElement('label');
+  labelNom.setAttribute('for', 'nom');
+  labelNom.textContent = 'Nom:';
+  form.appendChild(labelNom);
+  
+  let inputNom = document.createElement('input');
+  inputNom.setAttribute('type', 'text');
+  inputNom.setAttribute('id', 'nom');
+  inputNom.setAttribute('name', 'nom');
+  form.appendChild(inputNom);
+  
+  let labelPrenom = document.createElement('label');
+  labelPrenom.setAttribute('for', 'prenom');
+  labelPrenom.textContent = 'Prénom:';
+  form.appendChild(labelPrenom);
+  
+  let inputPrenom = document.createElement('input');
+  inputPrenom.setAttribute('type', 'text');
+  inputPrenom.setAttribute('id', 'prenom');
+  inputPrenom.setAttribute('name', 'prenom');
+  form.appendChild(inputPrenom);
+  
+  let labelTelephone = document.createElement('label');
+  labelTelephone.setAttribute('for', 'telephone');
+  labelTelephone.textContent = 'Téléphone:';
+  form.appendChild(labelTelephone);
+  
+  let inputTelephone = document.createElement('input');
+  inputTelephone.setAttribute('type', 'text');
+  inputTelephone.setAttribute('id', 'telephone');
+  inputTelephone.setAttribute('name', 'telephone');
+  form.appendChild(inputTelephone);
+  
+  let labelEmail = document.createElement('label');
+  labelEmail.setAttribute('for', 'email');
+  labelEmail.textContent = 'Email:';
+  form.appendChild(labelEmail);
+  
+  let inputEmail = document.createElement('input');
+  inputEmail.setAttribute('type', 'text');
+  inputEmail.setAttribute('id', 'email');
+  inputEmail.setAttribute('name', 'email');
+  form.appendChild(inputEmail);
+  
+  let submitButton = document.createElement('input');
+  submitButton.setAttribute('type', 'submit');
+  submitButton.setAttribute('value', 'Submit');
+  form.appendChild(submitButton);
+  
+  formbox.appendChild(form);
+  
+  return form;
+}
+
+function Open_formulaire() {
+  console.log("haha form")
+  // Get the .cart element
+  let form = document.querySelector(".formulaire");
+  // Add the class 'active' to the .cart element
+  form.classList.add("active")
+}
+
+function Close_formulaire() {
+  console.log("haha")
+  // Get the .cart element
+  let form = document.querySelector(".formulaire");
+  // Add the class 'active' to the .cart element
+  form.classList.remove("active");
+}
+
+
