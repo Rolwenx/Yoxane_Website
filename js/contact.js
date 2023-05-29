356833f1bcc7537c780fbdf261ffff7cf17fafc2
Rolwen <n.rnolwen@gmail.com>
2023-05-29 15:41:25 +0200

    contact page done (not responsive)

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,14 @@
-// JAVASCRIPT: adds a focus class when the user is selecting a field
-// And we remove it when the user isn't
 
 
 document.addEventListener("DOMContentLoaded", function() {
+
+    // FUNCTIONALITY: adds a focus class when the user is selecting a field, remove when don't
+
     // We select all our elements having the class "input"
     const inputs = document.querySelectorAll(".input");
-    console.log(inputs);
-
     /*When the input is focused by the user, we select its parent
     (the .input_field) and we add a focus to it (which will make
     the label go above the input field)*/
-
     function focus_Function(){
 
         let parent = this.parentNode;
@@ -23,12 +21,27 @@ document.addEventListener("DOMContentLoaded", function() {
         if(this.value == ""){
             parent.classList.remove("focus");
         }
-
     }
-
     inputs.forEach((input) => {
         input.addEventListener("focus", focus_Function);
         input.addEventListener("blur", defocus_Function);
     });
-  });
-  
+
+
+    // FUNCTIONALITY: Popup Message
+    const submitButton = document.querySelector(".submit_button");
+    submitButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        const usernameInput = document.querySelector('input[name="name"]');
+        const emailInput = document.querySelector('input[name="email"]');
+        const reasonInput = document.querySelector('select[name="reason"]');
+        const messageInput = document.querySelector('textarea[name="message"]');
+    
+        if (usernameInput.value === "" || emailInput.value === "" || messageInput.value === "" || reasonInput.value === "") {
+          alert("Please fill in all the required fields before sending the message.");
+          return;
+        } else {
+          popupAppears(event);
+        }
+      });
+    });
